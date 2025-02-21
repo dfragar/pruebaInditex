c6f8ccaf133f88d5d669f483a0b5b05847ee8f87
dfragar <dfragar.dev@gmail.com>
2025-02-21 14:14:49 +0100

    feat: add PriceNotFoundException for handling not found errors

---
+++ src/main/java/com/inditex/priceservice/domain/exception/PriceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.inditex.priceservice.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PriceNotFoundException extends RuntimeException {
+
+    public PriceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s not found with the given input data %s : '%s'", resourceName, fieldName,
+                fieldValue));
+    }
+
+}
